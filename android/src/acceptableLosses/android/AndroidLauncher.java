e44bab7c59cff680fb32eaf750b7f32749927a23
Adrian Moore <stewsters@gmail.com>
2014-11-20 18:12:07 -0600

    Adding libgdx

---
+++ android/src/acceptableLosses/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package acceptableLosses.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import acceptableLosses.AcceptableLossesGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AcceptableLossesGame(), config);
+	}
+}
---
+++ desktop/src/acceptableLosses/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package acceptableLosses.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import acceptableLosses.AcceptableLossesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AcceptableLossesGame(), config);
+	}
+}
