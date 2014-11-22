7b43a23968f3d703608d82fa73c6a3e107401b6a
Adrian Moore <stewsters@gmail.com>
2014-11-21 19:47:44 -0600

    center camera

--- core/src/acceptableLosses/screens/GameScreen.java
+++ core/src/acceptableLosses/screens/GameScreen.java
@@ -38,7 +38,7 @@ public class GameScreen implements Screen {
         shapeRenderer = new ShapeRenderer();
 
         camera = new OrthographicCamera();
-        camera.position.set(0, 0, 0);
+        camera.position.set(250, 250, 250);
         camera.zoom = 0.5f;
 
         hudCam = new OrthographicCamera();
